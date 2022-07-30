2f44279031839589da78f3be135880d093a9beb6
zohre khademi <zohrekhademi9312@gmail.com>
2022-07-30 16:05:45 +0430

    update sign up page

--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -6,7 +6,6 @@ import {notify} from './toast';
 import styles from './SignUp.module.css'
 
 
-
 const SignUp = () => {
 
     const [data , setData] = useState ({
@@ -61,32 +60,34 @@ const SignUp = () => {
                 <h2 className={styles.header}>Sign Up</h2>
                 <div className={styles.formField}>
                     <label>Name :</label>
-                    <input className={(errors.name && touched.name) ? styles.uncopleted : styles.formInput}
+                    <input className={(errors.name && touched.name) ? styles.uncompleted : styles.formInput}
                      type="text" name="name" value={data.name} onChange = {changeHandler} onFocus = {focusHandler}/>
                     {errors.name && touched.name &&<span>{errors.name}</span>}
                 </div>
                 <div className={styles.formField}>
                     <label>email :</label>
-                    <input className={(errors.email && touched.email) ? styles.uncopleted : styles.formInput}
+                    <input className={(errors.email && touched.email) ? styles.uncompleted : styles.formInput}
                      type="text" name="email" value={data.email} onChange = {changeHandler} onFocus = {focusHandler}/>
                     {errors.email && touched.email &&<span>{errors.email}</span>}
                 </div>
                 <div className={styles.formField}>
                     <label>password :</label>
-                    <input className={(errors.password && touched.password) ? styles.uncopleted : styles.formInput}
+                    <input className={(errors.password && touched.password) ? styles.uncompleted : styles.formInput}
                      type="password" name="password" value={data.password} onChange = {changeHandler} onFocus = {focusHandler}/>
                     {errors.password && touched.password &&<span>{errors.password}</span>}
                 </div>
                 <div className={styles.formField}>
                     <label>confirmPassword :</label>
-                    <input className={(errors.confirmPassword && touched.confirmPassword) ? styles.uncopleted : styles.formInput}
+                    <input className={(errors.confirmPassword && touched.confirmPassword) ? styles.uncompleted : styles.formInput}
                      type="password" name="confirmPassword" value={data.confirmPassword} onChange = {changeHandler} onFocus = {focusHandler}/>
                     {errors.confirmPassword && touched.confirmPassword &&<span>{errors.confirmPassword}</span>}
                 </div>
                 <div className={styles.formField}>
+                <div className={styles.checkboxContainer}>
                     <label>I accept terms of privacy policy </label>
-                    <input className={(errors.isAccepted && touched.isAccepted) ? styles.uncopleted : styles.formInput}
+                    <input
                      type="checkbox" name="isAccepted" value={data.isAccepted} onChange = {changeHandler} onFocus = {focusHandler}/>
+                    </div>
                     {errors.isAccepted && touched.isAccepted &&<span>{errors.isAccepted}</span>}
                 </div>
                 <div className={styles.formButton}>
